162d46027137048963658af20cd280031be991fa
yujin <kingyujin56@gmail.com>
2025-04-23 12:27:42 +0900

    feat: 로그인 로그아웃 버튼 추가

--- frontend/my-app/src/components/layout/Header.tsx
+++ frontend/my-app/src/components/layout/Header.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import ExitIcon from "../icons/ExitIcon";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { LoginBtn, LogoutBtn } from "../ui/auth/LoginBtn";
 
 const HeaderWrapper = styled.div`
   display: grid;
@@ -25,9 +26,11 @@ const PageTitle = styled.p`
 function Header() {
   const pathname = usePathname();
   const [isMounted, setIsMounted] = useState(false);
+  const [token, setToken] = useState<string | null>(null);
 
   useEffect(() => {
     setIsMounted(true);
+    setToken(localStorage.getItem("accessToken"));
   }, []);
 
   const titleMap: Record<string, string> = {
@@ -40,12 +43,18 @@ function Header() {
 
   const pageTitle = titleMap[pathname] || "*";
 
+  const deleteToken = () => {
+    localStorage.removeItem("accessToken");
+    window.location.reload();
+  };
+
   if (!isMounted) return null;
 
   return (
     <HeaderWrapper>
       <ExitIcon />
       <PageTitle>{pageTitle}</PageTitle>
+      {token ? <LogoutBtn onClick={deleteToken} /> : <LoginBtn />}
     </HeaderWrapper>
   );
 }
