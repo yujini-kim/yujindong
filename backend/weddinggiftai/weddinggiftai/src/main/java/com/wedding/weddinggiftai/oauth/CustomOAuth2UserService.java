b0df1b86d744da723dc4e399bdfaf0dc8968ff1e
ehdgnl1215 <ehdgnl1215@naver.com>
2025-05-03 14:59:54 +0900

    feat/kakao 로그인 기능 구현

--- backend/weddinggiftai/weddinggiftai/src/main/java/com/wedding/weddinggiftai/oauth/CustomOAuth2UserService.java
+++ backend/weddinggiftai/weddinggiftai/src/main/java/com/wedding/weddinggiftai/oauth/CustomOAuth2UserService.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -21,21 +22,42 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
-        String email = oAuth2User.getAttribute("email");
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // google or kakao
+        String email = null;
+        String displayName = null;
         String password = UUID.randomUUID().toString();
-        String displayName = oAuth2User.getAttribute("name");
 
 
-        Member member = memberRepository.findByEmail(email)
-                .orElseGet(() ->{
-                    Member m = new Member();
-                    m.setUsername(email);
-                    m.setPassword(password);
-                    m.setDisplayName(displayName);
-                    m.setEmail(email);
+        if (registrationId.equals("google")) {
+            // 구글 응답 구조
+            email = oAuth2User.getAttribute("email");
+            displayName = oAuth2User.getAttribute("name");
+        } else if (registrationId.equals("kakao")) {
+            // 카카오 응답 구조
+            // attributes -> kakao_account -> email, profile -> nickname
+            var kakaoAccount = (Map<String, Object>) oAuth2User.getAttributes().get("kakao_account");
+            var profile = (Map<String, Object>) kakaoAccount.get("profile");
 
-                    return memberRepository.save(m);
-                });
+            email = (String) kakaoAccount.get("email");
+            displayName = (String) profile.get("nickname");
+        } else {
+            throw new OAuth2AuthenticationException("Unsupported provider: " + registrationId);
+        }
+
+
+
+
+        Member member = memberRepository.findByEmail(email).orElse(null);
+
+        if (member == null) {
+            member = new Member();
+            member.setUsername(email);
+            member.setPassword(password);
+            member.setDisplayName(displayName);
+            member.setEmail(email);
+            member = memberRepository.save(member);
+        };
 
         return new CustomOAuth2User(member, oAuth2User.getAttributes());
     }
