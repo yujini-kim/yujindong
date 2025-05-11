6cc7f4f6c604bd148c4d37028b45db1a389fd21b
yujin <kingyujin56@gmail.com>
2025-05-11 18:06:41 +0900

    fix. 이미지 깜빡임 수정

--- frontend/my-app/src/components/Icon/MainIcon.tsx
+++ frontend/my-app/src/components/Icon/MainIcon.tsx
@@ -14,7 +14,7 @@ function MainIcon() {
         repeatType: "loop",
         ease: "easeInOut",
       }}
-      className="w-40 h-40 md:w-48 md:h-48"
+      className="relative w-40 h-40 md:w-48 md:h-48"
     >
       <Image
         src="/helloberger.svg"
