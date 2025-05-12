e186a00846287bee34bfcbdbe1e8284eeb8fec4e
yujin <kingyujin56@gmail.com>
2025-05-12 20:48:20 +0900

    feat: 메인페이지 내용 추가

--- frontend/my-app/src/app/page.tsx
+++ frontend/my-app/src/app/page.tsx
@@ -1,5 +1,11 @@
-"use client";
+import MainInfo from "@/components/main/main";
+import StartButton from "@/components/main/startButton";
 
 export default function Main() {
-  return <>메인페이지</>;
+  return (
+    <>
+      <MainInfo />
+      <StartButton />
+    </>
+  );
 }
