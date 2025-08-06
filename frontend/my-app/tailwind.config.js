a2c5093db87a506071cfbd1d1bdb66d806e46867
yujin <kingyujin56@gmail.com>
2025-08-06 21:04:07 +0900

    feat: tailwind 커스텀색상 세팅

--- frontend/my-app/tailwind.config.js
+++ frontend/my-app/tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './styles/**/*.{css}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+           primary: {
+          10: '#F9F7F3',
+          20: '#EFE9DF',
+          30: '#242020',
+          40: '#C4BCB0',
+          50: '#D9A844',
+        },
+        lightgray: {
+          10: '#F4F4F5',
+          20: '#E5E5E5',
+          30: '#C7C7C7',
+          40: '#C4BCB0',
+          50: '#D9A844',
+        },
+        accent: {
+          yellow: '#FAC656',
+          black: '#242020',
+          green: '#7BB274',
+          red: '#D95C5C',
+          blue: '#6A9DD3',
+          white: '#FFFFFF',
+        },
+      },
+    },
   },
   plugins: [],
 }
