88751e035d20c3b0f853932fee10f4465df3d50d
yujin <kingyujin56@gmail.com>
2025-08-06 21:02:18 +0900

    chore: taillwind v3으로 다운그레이드

--- frontend/my-app/src/app/page.tsx
+++ frontend/my-app/src/app/page.tsx
@@ -5,6 +5,8 @@ import TermsLink from "@/components/main/termsLink";
 export default function Main() {
   return (
     <>
+    <h1 className="bg-accent-red">test</h1>
+    <div className="size-10 bg-blue-200">d</div>
       <MainInfo />
       <StartButton />
       <footer className="w-full fixed bottom-2 flex justify-center items-center gap-12">
--- frontend/my-app/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  theme: {
-    extend: {
-      boxShadow: {
-        custom: "4px 4px 0px rgba(0, 0, 0, 0.3)",
-      },
-    },
-  },
-};
