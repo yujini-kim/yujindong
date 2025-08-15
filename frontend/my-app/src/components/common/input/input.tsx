5bc88b7bf10129eb01107f4cca6fae4f2061001f
yujin <kingyujin56@gmail.com>
2025-08-15 22:32:00 +0900

    feat: input 태그 클래스 추가

--- frontend/my-app/src/components/common/input/input.tsx
+++ frontend/my-app/src/components/common/input/input.tsx
@@ -1,18 +1,20 @@
+import { cn } from '@/lib/cn'
 import { InputHTMLAttributes } from 'react'
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
     fieldName: string
     title: string
+    className: string
 }
 
-export default function Input({ fieldName, title, ...rest }: Props) {
+export default function Input({ fieldName, title, className, ...rest }: Props) {
     return (
         <div className="flex flex-col gap-1">
             <label className="text-ink-800 font-semibold" htmlFor={fieldName}>
                 {title}
             </label>
             <input
-                className="bg-ink-200 px-4 py-3 rounded border focus:border-ink-800 focus:outline-none"
+                className={cn('bg-ink-200 px-4 py-3 rounded border focus:border-ink-800 focus:outline-none', className)}
                 id={fieldName}
                 {...rest}
             />
