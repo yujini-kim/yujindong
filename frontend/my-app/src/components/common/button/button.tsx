17c3e4a6da557b1391381f6f61b91ae23ee7e706
yujin <kingyujin56@gmail.com>
2025-08-21 18:41:49 +0900

    feat: button 컴포넌트 디자인 수정

--- frontend/my-app/src/components/common/button/button.tsx
+++ frontend/my-app/src/components/common/button/button.tsx
@@ -1,14 +1,12 @@
-import { cn } from '@/lib/cn'
+import { cn } from '@/utils/cn'
 import { cva, VariantProps } from 'class-variance-authority'
 import { ButtonHTMLAttributes } from 'react'
 
 const ButtonVariants = cva('flex items-center justify-center rounded-xl px-4 py-3', {
     variants: {
         variant: {
-            default: 'bg-brand-300 text-ink-900',
-            hover: 'bg-brand-400 text-ink-900',
-            enable: 'bg-ink-300 text-ink-500',
-            enableHover: 'bg-ink-400 text-ink-500',
+            default: 'bg-brand-200 text-ink-900 hover:bg-brand-300',
+            enable: 'bg-ink-300 text-ink-500 hover:bg-ink-400',
             accent: 'bg-brand-500 text-ink-900',
         },
     },
