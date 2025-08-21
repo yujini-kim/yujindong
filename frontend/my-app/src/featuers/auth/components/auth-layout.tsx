1954c6eccd2b1c669a716a8d15b3c7b5690bb983
yujin <kingyujin56@gmail.com>
2025-08-21 23:33:16 +0900

    feat: 반응형 디자인 구현

--- frontend/my-app/src/featuers/auth/components/auth-layout.tsx
+++ frontend/my-app/src/featuers/auth/components/auth-layout.tsx
@@ -9,10 +9,12 @@ interface Props {
 
 export default function AuthFormLayout({ children, footer }: Props) {
     return (
-        <div className="flex flex-col justify-center items-center gap-2 px-7">
-            <Logo className="w-52 h-28" />
-            {children}
-            <div className="mt-2">{footer}</div>
+        <div className="flex justify-center items-center">
+            <div className="w-[320px] md:w-[640px] flex flex-col justify-center items-center gap-2 ">
+                <Logo className="w-52 h-28" />
+                {children}
+                <div className="mt-2">{footer}</div>
+            </div>
         </div>
     )
 }
