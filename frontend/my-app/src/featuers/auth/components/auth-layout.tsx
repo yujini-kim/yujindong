6590648b04db6ee5fad15ec9b749a4623aa1a419
yujin <kingyujin56@gmail.com>
2025-08-22 16:42:15 +0900

    feat: 로고 클릭시 홈페이지로 이동

--- frontend/my-app/src/featuers/auth/components/auth-layout.tsx
+++ frontend/my-app/src/featuers/auth/components/auth-layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Logo } from '@/components/icon'
+import Link from 'next/link'
 
 interface Props {
     children: React.ReactNode
@@ -11,7 +12,9 @@ export default function AuthFormLayout({ children, footer }: Props) {
     return (
         <div className="flex justify-center items-center">
             <div className="w-[320px] md:w-[640px] flex flex-col justify-center items-center gap-2 ">
-                <Logo className="w-52 h-28" />
+                <Link href="/">
+                    <Logo className="w-52 h-28" />
+                </Link>
                 {children}
                 <div className="mt-2">{footer}</div>
             </div>
