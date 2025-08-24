2ba1e62e545f8b0077d84c3a3f36b8a1a4140c13
yujin <kingyujin56@gmail.com>
2025-08-24 22:20:53 +0900

    refactor: input 컴포넌트 수정ㅇ

--- frontend/my-app/src/app/analyze/page.tsx
+++ frontend/my-app/src/app/analyze/page.tsx
@@ -1,44 +1,11 @@
-"use client";
+'use client'
 
-import AnalyzeForm from "@/components/analyze/analyzeForm";
-import Loading from "@/components/result/Loading";
-import { useAnalyzeMutation } from "@/hooks/useAnalyzeMutation";
-import { useAnalyzeStore } from "@/store/AnalyzeStore";
-import { useRouter } from "next/navigation";
+import AnalyzeForm from '@/features/analyze/components/analyze-form'
 
 export default function Analyze() {
-  const analyzeMutation = useAnalyzeMutation();
-  const router = useRouter();
-  const { text, friend_name } = useAnalyzeStore();
-  const onSubmit = () => {
-    analyzeMutation.mutate(
-      { text, friend_name },
-      {
-        onSuccess: (result) => {
-          router.replace(`/result/${result.shareUuid}`);
-        },
-        onError: (err: any) => {
-          alert("분석에 실패했습니다: " + err.message);
-        },
-      }
-    );
-  };
-
-  return (
-    <>
-      <div className="mt-4 flex justify-center items-center relative min-h-[500px] lg:mt-10">
-        {analyzeMutation.isPending && (
-          <div className="absolute">
-            <Loading />
-          </div>
-        )}
-
-        <div
-          className={`${analyzeMutation.isPending ? "invisible" : "visible"}`}
-        >
-          <AnalyzeForm onSubmit={onSubmit} />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <AnalyzeForm />
+        </>
+    )
 }
--- frontend/my-app/src/components/common/input/input.tsx
+++ frontend/my-app/src/components/common/input/input.tsx
@@ -1,25 +1,45 @@
 import { cn } from '@/utils/cn'
+import { cva, VariantProps } from 'class-variance-authority'
 import { InputHTMLAttributes, ReactNode } from 'react'
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
-    title: string
+const InputVariants = cva('w-full px-4 py-3 rounded focus:border-ink-800 focus:outline-none pr-10', {
+    variants: {
+        inputType: {
+            file: 'hidden',
+            text: 'bg-ink-200 border',
+        },
+    },
+    defaultVariants: {
+        inputType: 'text',
+    },
+})
+
+const LabelVariants = cva('text-sm font-medium text-ink-900', {
+    variants: {
+        inputType: {
+            file: 'cursor-pointer border border-ink-900',
+            text: '',
+        },
+    },
+})
+
+interface Props extends InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof InputVariants> {
+    title?: string
     fieldName: string
     icon?: ReactNode
 }
 
-export default function Input({ title, fieldName, icon, className, ...rest }: Props) {
+export default function Input({ title, fieldName, type, inputType, icon, className, ...rest }: Props) {
     return (
         <div className="w-full flex flex-col gap-1">
-            <label htmlFor={fieldName} className="text-sm font-medium text-ink-900">
+            <label htmlFor={fieldName} className={cn(LabelVariants({ inputType: type === 'file' ? 'file' : 'text' }))}>
                 {title}
             </label>
             <div className="relative">
                 <input
                     id={fieldName}
-                    className={cn(
-                        'w-full bg-ink-200 px-4 py-3 rounded border focus:border-ink-800 focus:outline-none pr-10', // pr-10 오른쪽 여백 확보
-                        className,
-                    )}
+                    type={type}
+                    className={cn(InputVariants({ inputType: type === 'file' ? 'file' : 'text' }), className)}
                     {...rest}
                 />
                 {icon && <span className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer">{icon}</span>}
