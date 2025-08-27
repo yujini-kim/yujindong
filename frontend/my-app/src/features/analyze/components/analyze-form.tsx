f5e954c96b718ab797b15dd28ce759f1a6052963
yujin <kingyujin56@gmail.com>
2025-08-27 23:49:34 +0900

    feat: 파일업로드 기능 구현

--- frontend/my-app/src/features/analyze/components/analyze-form.tsx
+++ frontend/my-app/src/features/analyze/components/analyze-form.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Button from '@/components/common/button/button'
 import Input from '@/components/common/input/input'
 import { useForm } from 'react-hook-form'
@@ -9,16 +10,29 @@ interface analyzeFormData {
 }
 
 export default function AnalyzeForm() {
-    const { register, handleSubmit } = useForm<analyzeFormData>()
+    const { register, handleSubmit, setValue, watch } = useForm<analyzeFormData>()
     const analyzeMutation = useAnalyzeMutation()
     const onSubmit = (formData: analyzeFormData) => {
         analyzeMutation.mutate(formData)
     }
+    const textValue = watch('text', '')
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0]
+        if (!file) return
+
+        const reader = new FileReader()
+        reader.onload = () => {
+            setValue('text', reader.result as string, { shouldValidate: true })
+        }
+        reader.readAsText(file)
+    }
+
     return (
         <div className="p-2">
             <form onSubmit={handleSubmit(onSubmit)} className="w-full  flex flex-col justify-center items-center">
-                <Input title="파일업로드하기" fieldName="file" type="file" accept=".txt" />
-                <Input fieldName="text" title="분석할 대화내용" {...register('text')} />
+                <Input title="파일업로드하기" fieldName="file" type="file" accept=".txt" onChange={handleFileChange} />
+                <Input fieldName="text" title="분석할 대화내용" {...register('text')} value={textValue} />
                 <Input fieldName="friendName" title="친구이름" {...register('friend_name')} />
                 <Button>분석하기</Button>
             </form>
