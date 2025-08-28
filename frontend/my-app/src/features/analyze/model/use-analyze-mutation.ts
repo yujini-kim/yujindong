334c9bdb17ca8987d589b6ca037338a04ad6c248
yujin <kingyujin56@gmail.com>
2025-08-28 17:38:10 +0900

    feat: 분석 api-수정 분석 완료 후 페이지 이동

--- frontend/my-app/src/features/analyze/model/use-analyze-mutation.ts
+++ frontend/my-app/src/features/analyze/model/use-analyze-mutation.ts
@@ -1,13 +1,18 @@
+'use client'
+
 import { toast } from '@/components/common/toast/toast'
 import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
 import { postAnalyzeAPI } from '../api/post-analyze.API'
 
 export const useAnalyzeMutation = () => {
+    const router = useRouter()
     const mutation = useMutation({
         mutationFn: postAnalyzeAPI,
         onSuccess: (data) => {
             toast.success(`분석완료`)
             console.log(data)
+            router.push(`result/${data.shareUuid}`)
         },
         onError: (err) => {
             toast.error(err.message)
