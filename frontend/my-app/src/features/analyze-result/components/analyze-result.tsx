ae730286044ae1e5a92eb17d5a97fb737083f48c
yujin <kingyujin56@gmail.com>
2025-08-29 23:24:04 +0900

    feat: 카카오 공유하기 기능 구현

--- frontend/my-app/src/features/analyze-result/components/analyze-result.tsx
+++ frontend/my-app/src/features/analyze-result/components/analyze-result.tsx
@@ -2,16 +2,18 @@
 
 import Button from '@/components/common/button/button'
 import { toast } from '@/components/common/toast/toast'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import { useAnalyzeResultQuery } from '../model/use-analyze-result-query'
 import InfoCard from './info-card'
 import ResultSkeleton from './result-skeleton'
 import ScoreCard from './score-card'
+import ShareAnalyzeResult from './share-analyze-result'
 import SummaryCard from './summary-card'
 import TextCard from './text-card'
 
 export default function AnalyzeResult() {
     const { uuid } = useParams<{ uuid: string }>()
+    const router = useRouter()
 
     if (!uuid) {
         toast.error('오류가 발생했습니다')
@@ -20,32 +22,31 @@ export default function AnalyzeResult() {
 
     const { data, isLoading, isError } = useAnalyzeResultQuery(uuid)
 
-    if (isLoading) {
-        return <ResultSkeleton />
-    }
-
+    if (isLoading) return <ResultSkeleton />
     if (isError || !data) {
         toast.error('분석 결과를 불러오지 못했습니다')
         return null
     }
 
+    const restart = () => router.back()
+
     return (
-        <div className="flex flex-col lg:flex-row items-center justify-center gap-4">
+        <div className="flex flex-col items-center justify-center gap-4">
+            <div className="flex flex-col lg:flex-row gap-4">
+                <div className="flex gap-4">
+                    <ScoreCard score={Number(data.score ?? 0)} />
+                    <SummaryCard summary={data.summary} />
+                </div>
+                <div>
+                    <InfoCard friendName={data.friendName} recommendation={data.recommendation} />
+                    <TextCard text={data.text} />
+                </div>
+            </div>
             <div className="flex gap-4">
-                <ScoreCard score={Number(data.score ?? 0)} />
-                <SummaryCard summary={data.summary} />
-            </div>
-            <div>
-                <InfoCard friendName={data.friendName} recommendation={data.recommendation} />
-                <TextCard text={data.text} />
-            </div>
-            <div className="flex gap-2">
-                <Button variant="accent" className="w-40">
+                <Button onClick={restart} variant="ink" className="w-40 lg:w-[336px]">
                     다시하기
                 </Button>
-                <Button variant="border" className="w-40">
-                    공유하기
-                </Button>
+                <ShareAnalyzeResult data={data} uuid={uuid} />
             </div>
         </div>
     )
